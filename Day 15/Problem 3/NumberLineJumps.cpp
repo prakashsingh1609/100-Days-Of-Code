20c15252c1d6d90ef60e86784de1dc321cb20d64
Prakash Singh <57653552+prakashsingh1609@users.noreply.github.com>
2021-06-15 17:20:52 +0530

    HackerRank

---
+++ Day 15/Problem 3/NumberLineJumps.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int x2,x1,v1,v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    if((x2-x1) * (v2-v1) < 0 && (x2-x1)%(v2-v1) == 0)
+    {
+        cout << "YES";
+    }
+    else 
+    
+    {
+        cout << "NO";
+    }
+    
+}
