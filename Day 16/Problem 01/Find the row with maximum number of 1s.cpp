efe834f7cd60d2040cf17796f8fb7acd00c9ae58
Prakash Singh <57653552+prakashsingh1609@users.noreply.github.com>
2021-06-16 21:33:31 +0530

    GFG
    https://www.geeksforgeeks.org/find-the-row-with-maximum-number-1s/

---
+++ Day 16/Problem 01/Find the row with maximum number of 1s.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define R 4
+#define C 4
+
+int main() 
+{
+    int max=0,r=0;
+   bool mat[R][C] = { {0, 0, 0, 1},
+                    {0, 1, 1, 1},
+                    {1, 1, 1, 1},
+                    {0, 0, 0, 0}};
+    for(int i=0;i<R;i++)
+    {   int c=0;
+        for(int j=0;j<C;j++)
+        {
+            if(mat[i][j]==1)
+            {
+                c++;
+            }
+        }
+        if(c>max)
+        {
+            max=c;
+            //cout<<max<<" ";
+            r=i;
+            //cout<<r;
+            
+        }
+    }
+    
+    cout<<r;
+    return 0;
+}
