82cca966ee8418fe3ca06b9ba0c89a5e3c965d29
Prakash Singh <57653552+prakashsingh1609@users.noreply.github.com>
2021-06-25 20:21:37 +0530

    GFG

---
+++ Day 25/Problem 1/IshanLovesChocolates.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int chocolates(int arr[], int n);
+
+
+int main()
+{
+    
+    int t;cin>> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        
+        cout << chocolates(arr, n);
+        cout << endl;
+        
+    }
+
+}
+// } Driver Code Ends
+
+
+int chocolates(int arr[], int n)
+{
+    // Complete the function
+    int min = arr[0];
+    for(int i = 0; i<n; i++)
+    {
+        if(min>arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+    
+}
