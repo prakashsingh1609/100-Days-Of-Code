0381d1beef179258908489ebffcef6e0db89f363
Prakash Singh <57653552+prakashsingh1609@users.noreply.github.com>
2021-07-30 22:44:49 +0530

    Leetcode

---
+++ Day 60/SearchInABST.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root)return NULL;
+        TreeNode *node=new TreeNode();
+        if(root->val==val){
+            node=root;
+            return node;
+        }
+        if(root->val>val)node=searchBST(root->left,val);
+        else node=searchBST(root->right,val);
+        return node;
+    }
+};
